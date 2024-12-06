a0bdafd4d405093d511d22bd9aacc08d70872964
Akram <akram94407@gmail.com>
2024-12-06 20:39:56 +0530

    test 1

--- src/main/java/com/sample/entity/Student.java
+++ src/main/java/com/sample/entity/Student.java
@@ -14,6 +14,38 @@ public class Student {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
     @Column(name = "name", nullable = false)
     private String name;
 
